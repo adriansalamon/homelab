fe911d4a9c61cac0e62b3b74edb92237d180d7e5
Adrian Salamon <adr.salamon@gmail.com>
2025-10-29 20:25:07 +0100

    encrypt config secrets for ci

--- hosts/nixos/demeter/disk-config.secret.nix
+++ hosts/nixos/demeter/disk-config.secret.nix
@@ -1,15 +1,15 @@
 age-encryption.org/v1
--> piv-p256 wnNcqA Aqsfys8/tavV4rE+mr7UEShIVm+NBNRFaBVPzIwO5uuz
-ftCzvWl/Hg0Q0MMCylX9W1xiB27uclwfDBTIGHNQn0o
--> piv-p256 1IZxXQ A9WR0BsdvJHMvboXomlb+Iv3ZMO58O8Ln7u6DSMfVTPS
-w6r+E29TfcELh959QirhC8oKGIohAk9/8X1WHgIzh98
--> piv-p256 wnNcqA AtjSXy/ewHRhpDhoPn/C5I9EQikgNSzVkn+HGWK99XyJ
-NDtwa5EWG1W/Jj79brwJVS7pFbhVWg5JTMfyU1T6KVw
--> piv-p256 1IZxXQ Ax96O1UzpyZjXhS9+w49ENtkkuN1QoZ14lNC1XKzDTxY
-3zQxmDikxbk2dmerbQm7nWa1OFvPzDteO4YDp31jPC8
--> k{d&)-grease
-2eDaelcsCI2CLDE4HCQrt2qNBCGaB09wy96juSO/eNUfAR6ymqx8XrVUypIXCvMg
-r5kOxQr2CJIEBp29sufA9ATeMxIrt7TVQD7eO27mc9q9YHYiDpUZhfeVR370
---- sieSQf7uzZNIhTalYoApzvK3gFhXPWGvrkikDHfmbcc
-­HÊœÀå‘vî²ìåDwjU-A¹d“ó[¶†žË¼	FÔÿ)92¿gyÉÅ6 ”-e¦7.»~úÀ=2
-øèNúñâv7àX€`ëÚ~Ùc¯xÞáX?æ“Ïa£ÍfMÈâ×~˜èeFbðn©[Vh)£òZ­æ!U|mÝ“j·¨Üs{s© íu^F5±Ô÷=Se¨46ë¥ÝMã1öø/28©VdLþK¡´,Î;7²4Aå|À#†m?„xð¶I'ÌÎ <,ê%Òe¯Ç
+-> ssh-ed25519 ccY0rQ T5vWXYEPP8KF/oDCZ4vYKpzl6YDcTmx2d/M0iEBaj0U
+Ap2s8hoHM1TsbftN+dPllv1AdqKW4wjmrZjYK1MJas4
+-> piv-p256 wnNcqA AkSdaqKqxUu0EPycghxn2qtOSX8uaYOPzYOiNZTyfFnG
+E+eUqVaruyWFiDa9MYkXKki3J4Ifz7ZVkVhbk9iWCnM
+-> piv-p256 1IZxXQ AvO01BG+9fWV9mmFNYMMnWN226ZdYSXCBBDPAZQDy9it
+IqUgLbatGQLkznjKOmirok3dsc/F7sZUi9O6jc4D3Ys
+-> piv-p256 wnNcqA AkuS/cD8UH9jV8+lRDsBUE9zmkOKdM9XcwLJo9HaOFtg
++m0TKGzrJtu8qT6u047ne7MWarvnlM7RajkcoKAA1hU
+-> piv-p256 1IZxXQ Ak6xbOls1B0fXA1M1ycLjU6DryXm4iQZBsOqceScHhQ8
+ZpcTFaEFaxqmAjN3vqTBJfJa7Hu2O33jKax3CJWXo+8
+-> Ubc-grease v#^#2- <6p5fqs) =@ur*c=u
+GWdOqR/stw
+--- IItTnbahuoOaH3ZZ5lprXQmNwgw6V7VKvQQzHHEbtP8
+ÑõJp˜bÂ´ÝšÁÇ*ÏYXáõ&xpUv„…Á÷ÃjOJ#¸+Ó>*5±êPÇ¤=m6BNš„Í›Î·$Öyïõ07KÝS–‹J°F'Â=´×«'òÑƒÓæjÑ0+hA1ånÌ#˜œ 4Nc;¥vç7K>·g[|)krÓRœò~òc¦A+…_:~Ãý¿gUíšWÐÓ€3¹Ø6‰7óÍÈ{¶þÛM`nDŸÌ„î2çåIÇÖn›Ÿ#-ßÀ!í‰Ò2oâ-'è’¶æ6ê+c‰ßÝœW
