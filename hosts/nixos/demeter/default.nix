e45048337cac911d183ddec5d74d1a76f2e289e5
Adrian Salamon <adrian@salamon.se>
2026-04-18 21:06:17 +0200

    mark nix builders

--- hosts/nixos/demeter/default.nix
+++ hosts/nixos/demeter/default.nix
@@ -47,11 +47,17 @@
       group = "root";
       mode = "0755";
     }
+    {
+      directory = "/var/cache/nix-eval";
+      user = "root";
+      group = "root";
+      mode = "0755";
+    }
   ];
 
-  services.forgejo.runner.instances.homelab = {
+  services.forgejo.runner.instances.docker = {
     enable = true;
-    name = config.node.name;
+    name = "${config.node.name}";
     url = "https://forgejo.${globals.domains.main}";
     tokenFile = config.age.secrets.forgejo-runner-token.path;
     labels = [
@@ -67,7 +73,48 @@
         # Where to store the registration result.
         file = ".runner";
         # Execute how many tasks concurrently at the same time.
-        capacity = 2;
+        capacity = 5;
+        envs = { };
+        timeout = "3h";
+        shutdown_timeout = "5m";
+        insecure = false;
+        fetch_timeout = "5s";
+        fetch_interval = "2s";
+        report_interval = "1s";
+      };
+      cache = {
+        enabled = true;
+        dir = "/var/lib/forgejo-runner/cache";
+      };
+
+      container = {
+        network = "bridge";
+        privileged = true;
+        docker_host = "automount";
+        valid_volumes = [ ];
+        options = lib.concatStringsSep " " [ ];
+      };
+    };
+  };
+
+  services.forgejo.runner.instances.nix = {
+    enable = true;
+    name = "nix_${config.node.name}";
+    url = "https://forgejo.${globals.domains.main}";
+    tokenFile = config.age.secrets.forgejo-runner-token.path;
+    labels = [
+      "nix:docker://ghcr.io/catthehacker/ubuntu:act-latest"
+    ];
+    settings = {
+      log = {
+        level = "info";
+      };
+
+      runner = {
+        # Where to store the registration result.
+        file = ".runner";
+        # Execute how many tasks concurrently at the same time.
+        capacity = 1;
         envs = { };
         timeout = "3h";
         shutdown_timeout = "5m";
@@ -90,6 +137,7 @@
           "/nix/var/nix/daemon-socket"
           "/run/current-system/sw/bin/nix"
           "/etc/nix/ci-nix.conf"
+          "/var/cache/nix-eval"
           "${pkgs.sops}/bin/sops"
           "${pkgs.attic-client}/bin/attic"
           config.age.secrets.nix-cache-netrc.path
@@ -102,6 +150,7 @@
           "-v ${pkgs.attic-client}/bin/attic:/usr/local/bin/attic:ro"
           "-v /etc/nix/ci-nix.conf:/etc/nix/nix.conf:ro"
           "-v ${config.age.secrets.nix-cache-netrc.path}:${config.age.secrets.nix-cache-netrc.path}:ro"
+          "-v /var/cache/nix-eval:/root/.cache/nix"
           "-e NIX_REMOTE=daemon"
         ];
       };
